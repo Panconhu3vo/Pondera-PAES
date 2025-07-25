463bb269e0a2904db677490730a7cc3d152f57df
panconhu3vo <panconhuevo729@gmail.com>
2025-07-25 08:38:27 -0400

    feat(fonts):Agrega font Raleway

---
+++ Pondera-PAES/src/constants/Fonts.js
@@ -0,0 +1,10 @@
+import {
+    Raleway_400Regular,
+    Raleway_700Bold,
+    Raleway_300Light,
+} from "@expo-google-fonts/raleway";
+
+export const CUSTOM_FONTS = {
+    "Raleway-Regular": require("@expo-google-fonts/raleway").Raleway_400Regular,
+    "Raleway-Bold": require("@expo-google-fonts/raleway").Raleway_700Bold,
+};
