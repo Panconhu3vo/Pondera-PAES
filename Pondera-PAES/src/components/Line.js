8e0d487fcf00071234a2c50b947b268b93637817
panconhu3vo <panconhuevo729@gmail.com>
2025-07-25 12:04:00 -0400

    feat(component): Agrega componente Line

---
+++ Pondera-PAES/src/components/Line.js
@@ -0,0 +1,23 @@
+import { View, StyleSheet } from "react-native";
+import color from "../constants/Colores";
+export default function Line() {
+    return (
+        <View
+            style={{
+                alignItems: "center",
+                justifyContent: "center",
+                margin: 1,
+            }}>
+            <View style={styles.line}></View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    line: {
+        borderBottomColor: color.terciario,
+        borderBottomWidth: 2,
+        width: 270,
+        borderRadius: 10,
+    },
+});
