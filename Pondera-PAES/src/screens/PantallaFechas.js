2be4fbf61f415c99d51a2ed5e48a0c25f3ce74cb
thiareecart <thiare.carvacho.cornejo@alumnos.cmch.maristas.cl>
2025-08-04 10:38:52 -0400

    feat: Fechas

--- Pondera-PAES/src/screens/PantallaFechas.js
+++ Pondera-PAES/src/screens/PantallaFechas.js
@@ -1,9 +1,26 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import fechas from "../../data/Fechas.json";
+import { useState, useEffect } from "react";
+import DateBlock from "../components/DateBlock";
 
 export default function PantallaFechas() {
+    const [fecha, setFecha] = useState([]);
+
+    useEffect(() => {
+        if (fechas) {
+            setFecha = fechas;
+        } else {
+            console.log("No se encontro el archivo");
+        }
+    }, []);
+
     return (
         <View>
-            <Text> PantallaFechas</Text>
+            <FlatList
+                data={fecha}
+                keyExtractor={(item, index) => item.año + "_" + index}
+                renderItem={<DateBlock dia={item.dia}mes={item.mes}descripcion={item.descripcion} />}
+            />
         </View>
     );
 }
